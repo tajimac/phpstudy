40a55a1e16dffc2a7a0649e70e56f4e3dcacb143
hibiki <h.tajima5050@gmail.com>
2014-10-05 14:13:36 +0900

    thank you

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+ 
+ //条件分岐（if）
+ //比較演算子<><= >= == !=
+ //論里演算子 &&(ABD) ||(OR) !(NOT)
+ 
+ $age = 10;
+ 
+/*
+if($age > 20){
+    $message = "大人！";
+}else{
+   $message = "子供！";
+}
+/*
+
+
+$message = ($age > 20) ? "大人！" : "子供！";
+echo =$message;
